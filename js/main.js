2eea3ea20022a71410034ecb03229f4887020aa2
Emilio Bensignor <liobensignor@gmail.com>
2022-12-07 12:03:16 -0300

    peuXdia

--- js/main.js
+++ js/main.js
@@ -28,6 +28,8 @@ botonJavaTik.addEventListener('click', diasJT)
 botonJavaKava.addEventListener('click', diasJK)
 botonJavaJalo.addEventListener('click', diasJJ)
 
+const sectionPeula = document.getElementById('items')
+
 var seccion = document.getElementById("seccion");
 seccion.style.display = 'none'
 
@@ -90,6 +92,255 @@ function variantes (dia) {
             cronograma = './fotitos/cronoJKcorto.png'
         } else {
             cronograma = './fotitos/cronoJKlargo.png'
+        }
+        
+    }
+    if (dia == 18) {
+        if (cohadraja == 'Kava') {
+            sectionPeula.innerHTML = `
+            <div class="divPeulot">
+                <a href="#">Peula 1 Nitsim</a>
+            </div>
+            <div class="divGrales">
+                <a href="#">Generales</a>
+            </div>
+        `
+        } else {
+            sectionPeula.innerHTML = `
+            <div class="divGrales">
+                <a href="#">Generales</a>
+            </div>
+        `
+        }
+    } else if (dia == 19) {
+        if (cohadraja == 'Kava') {
+            sectionPeula.innerHTML = `
+            <div class="divPeulot">
+                <a href="#">Peula 1 Bala</a>
+                <a href="#">Peula 2 Nitsim</a>
+                <a href="#">Peula 2 Bala</a>
+            </div>
+            <div class="divGrales">
+                <a href="#">Generales</a>
+            </div>
+        `
+        } else {
+            sectionPeula.innerHTML = `
+            <div class="divPeulot">
+                <a href="#">Peula Sala de 4</a>
+                <a href="#">General Java</a>
+                <a href="#">Laila tov Sala de 5</a>
+            </div>
+            <div class="divGrales">
+                <a href="#">Generales</a>
+            </div>
+        `
+        }
+    } else if (dia == 20) {
+        if (cohadraja == 'Kava') {
+            sectionPeula.innerHTML = `
+            <div class="divPeulot">
+                <a href="#">Peula de Cierre</a>
+            </div>
+            <div class="divGrales">
+                <a href="#">Generales</a>
+            </div>
+        `
+        } else {
+            sectionPeula.innerHTML = `
+            <div class="divPeulot">
+                <a href="#">Peula Sala de 5</a>
+                <a href="#">Peula Sala de 3</a>
+            </div>
+            <div class="divGrales">
+                <a href="#">Generales</a>
+            </div>
+        `
+        }
+    } else if (dia == 21) {
+        if (cohadraja == 'Tik') {
+            sectionPeula.innerHTML = `
+            <div class="divPeulot">
+                <a href="#">Peula 1 Kofim-Barda</a>
+                <a href="#">Laila tov Iotza</a>
+            </div>
+            <div class="divGrales">
+                <a href="#">Generales</a>
+            </div>
+        `
+        } else {
+            sectionPeula.innerHTML = `
+            <div class="divPeulot">
+                <a href="#">Peula 1 5to</a>
+                <a href="#">Peula 2 Jaza</a>
+                <a href="#">Laila tov Amor</a>
+            </div>
+            <div class="divGrales">
+                <a href="#">Generales</a>
+            </div>
+        `
+        }
+    } else if (dia == 22) {
+        if (cohadraja == 'Tik') {
+            sectionPeula.innerHTML = `
+            <div class="divPeulot">
+                <a href="#">Peula 2 Iotza-Keisa</a>
+                <a href="#">Peula 3 Loja-Jibu</a>
+                <a href="#">Laila tov Barda</a>
+            </div>
+            <div class="divGrales">
+                <a href="#">Generales</a>
+            </div>
+        `
+        } else {
+            sectionPeula.innerHTML = `
+            <div class="divPeulot">
+                <a href="#">Peula 3 Amor</a>
+                <a href="#">Peula 4 Ofek</a>
+                <a href="#">Laila tov Kosmim</a>
+            </div>
+            <div class="divGrales">
+                <a href="#">Generales</a>
+            </div>
+        `
+        }
+    } else if (dia == 23) {
+        if (cohadraja == 'Kava') {
+            sectionPeula.innerHTML = `
+            <div class="divPeulot">
+                <a href="#">Peula 1</a>
+            </div>
+            <div class="divGrales">
+                <a href="#">Generales</a>
+            </div>
+        `
+        } else if (cohadraja == 'Tik') {
+            sectionPeula.innerHTML = `
+            <div class="divPeulot">
+                <a href="#">Peula 4 Kofim-Loja</a>
+                <a href="#">Peula 5 Barda-Iotza</a>
+                <a href="#">Laila tov Loja</a>
+            </div>
+            <div class="divGrales">
+                <a href="#">Generales</a>
+            </div>
+        `
+        } else {
+            sectionPeula.innerHTML = `
+            <div class="divPeulot">
+                <a href="#">Peula 5 Kosmim</a>
+                <a href="#">Peula 6 5to</a>
+                <a href="#">Laila tov Ofek</a>
+            </div>
+            <div class="divGrales">
+                <a href="#">Generales</a>
+            </div>
+        `
+        }
+    } else if (dia == 24) {
+        if (cohadraja == 'Kava') {
+            sectionPeula.innerHTML = `
+            <div class="divPeulot">
+                <a href="#">Peula 2</a>
+                <a href="#">Laila toc</a>
+            </div>
+            <div class="divGrales">
+                <a href="#">Generales</a>
+            </div>
+        `
+        } else if (cohadraja == 'Tik') {
+            sectionPeula.innerHTML = `
+            <div class="divPeulot">
+                <a href="#">Peula 6 Jibu-Keisa</a>
+            </div>
+            <div class="divGrales">
+                <a href="#">Generales</a>
+            </div>
+        `
+        } else {
+            sectionPeula.innerHTML = `
+            <div class="divPeulot">
+                <a href="#">Boker tov 5to</a>
+                <a href="#">Peula 7 6to</a>
+            </div>
+            <div class="divGrales">
+                <a href="#">Generales</a>
+            </div>
+        `
+        }
+    } else if (dia == 25) {
+        if (cohadraja == 'Kava') {
+            sectionPeula.innerHTML = `
+            <div class="divPeulot">
+                <a href="#">Peula 3</a>
+                <a href="#">Momento Java</a>
+            </div>
+            <div class="divGrales">
+                <a href="#">Generales</a>
+            </div>
+        `
+        } else if (cohadraja == 'Tik') {
+            sectionPeula.innerHTML = `
+            <div class="divPeulot">
+                <a href="#">Peula 7 Tercero</a>
+            </div>
+            <div class="divGrales">
+                <a href="#">Generales</a>
+            </div>
+        `
+        } else {
+            sectionPeula.innerHTML = `
+            <div class="divPeulot">
+                <a href="#">Peula 8 5to</a>
+                <a href="#">Laila tov Jaza</a>
+            </div>
+            <div class="divGrales">
+                <a href="#">Generales</a>
+            </div>
+        `
+        }
+    } else if (dia == 26) {
+        if (cohadraja == 'Kava') {
+            sectionPeula.innerHTML = `
+            <div class="divPeulot">
+                <a href="#">Peula 4</a>
+            </div>
+            <div class="divGrales">
+                <a href="#">Generales</a>
+            </div>
+        `
+        } else if (cohadraja == 'Tik') {
+            sectionPeula.innerHTML = `
+            <div class="divPeulot">
+                <a href="#">Peula 8 Cuarto</a>
+            </div>
+            <div class="divGrales">
+                <a href="#">Generales</a>
+            </div>
+        `
+        } else {
+            sectionPeula.innerHTML = `
+            <div class="divPeulot">
+                <a href="#">Peula 9 7mo</a>
+            </div>
+            <div class="divGrales">
+                <a href="#">Generales</a>
+            </div>
+        `
+        }
+    } else if (dia == 27) {
+        if (cohadraja == 'Tik') {
+            sectionPeula.innerHTML = `
+            <div class="divPeulot">
+                <a href="#">Peula de cierre</a>
+            </div>
+        `
+        } else {
+            sectionPeula.innerHTML = `
+            <div class="divPeulot">
+                <a href="#">Peula de cierre</a>
+            </div>
+        `
         }
     }
     nombre.innerHTML = `<h2 class="subtitulo">Javaia ${cohadraja} - Día ${dia}</h2>`
